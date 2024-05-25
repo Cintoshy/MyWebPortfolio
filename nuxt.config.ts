6b69a5f34f59f03cc8a1fedc831715a3cde3c76f
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-05-25 23:33:24 +0800

    update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,20 @@ export default defineNuxtConfig({
       title: "Mark Louis Odavar",
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      meta: [
+        {
+          name: "description",
+          content: "Mark Louis Odavar's professional portfolio",
+        },
+        {
+          name: "keywords",
+          content: "Mark Louis Odavar, portfolio, web developer, projects",
+        },
+        { name: "author", content: "Mark Louis Odavar" },
+        { name: "robots", content: "index, follow" },
+      ],
     },
+
     pageTransition: { name: "page", mode: "out-in" },
   },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/motion/nuxt", "nuxt-swiper"],
